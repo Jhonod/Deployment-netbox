1653ae9c50445c35e8f80feb66554df97bf5408d
Jhonod <akmjonod@gmail.com>
2025-07-10 14:03:14 +0700

    test

--- main.tf
+++ main.tf
@@ -16,8 +16,8 @@ data "google_cloud_run_service" "netbox" {
 }
 
 resource "google_cloud_run_service_iam_member" "all_users" {
-  location = resource.google_cloud_run_service.netbox.location
-  service  = resource.google_cloud_run_service.netbox.name
+  location = google_cloud_run_service.netbox.location
+  service  = google_cloud_run_service.netbox.name
   role     = "roles/run.invoker"
   member   = "allUsers"
 }
